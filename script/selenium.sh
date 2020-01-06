647eef90848a8d2a129122311551771c4fc396e2
RunAtWorld <hudalpf@163.com>
2020-01-06 14:26:59 +0000

    Add new file

---
+++ script/selenium.sh
@@ -0,0 +1,3 @@
+#安装 selenium
+pip install -i https://mirrors.aliyun.com/pypi/simple selenium --trusted-host mirrors.aliyun.com
+pip install selenium -i http://pypi.douban.com/simple --trusted-host pypi.douban.com
