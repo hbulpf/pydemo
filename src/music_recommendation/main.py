7140f77c2ed969de3ba3f7c69b5b620c64e673ef
lipengfei <hudalpf@163.com>
2020-02-14 18:03:49 +0800

    python flask

--- src/music_recommendation/main.py
+++ src/music_recommendation/main.py
@@ -1,6 +1,5 @@
 import os
 from flask import Flask, request, render_template, Markup
-from model import predict
 
 app = Flask(__name__)
 
@@ -10,11 +9,18 @@ def demo():
     return render_template('index.html', input_text = '', res_text = '')
   else:
     inputText = request.form.get("input_text")
-    resText = formatRes(predict(str(inputText)))
+    resText = Markup(formatRes(reverseText(inputText)))
     return render_template('index.html', input_text = inputText, res_text = resText)
 
 def formatRes(textList):
   return '<p>' + '</p><p>'.join(textList) + '</p>'
+
+# A sample
+def reverseText(text):
+  res = []
+  res.append('Original text: %s' %(text))
+  res.append('Converted text: %s' %(''.join(reversed(list(text)))))
+  return res
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port='5000')
