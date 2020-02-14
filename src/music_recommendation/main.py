6b845016bf067b0ed2a8dcb5742ff14c15b76f07
scnulpc <40417289+scnulpc@users.noreply.github.com>
2020-02-14 11:35:04 +0800

    Update main.py
    加载模型进行推荐

--- src/music_recommendation/main.py
+++ src/music_recommendation/main.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, request, render_template, Markup
+from model import predict
 
 app = Flask(__name__)
 
@@ -9,18 +10,11 @@ def demo():
     return render_template('index.html', input_text = '', res_text = '')
   else:
     inputText = request.form.get("input_text")
-    resText = Markup(formatRes(reverseText(inputText)))
+    resText = predict(str(inputText))
     return render_template('index.html', input_text = inputText, res_text = resText)
 
 def formatRes(textList):
   return '<p>' + '</p><p>'.join(textList) + '</p>'
-
-# A sample
-def reverseText(text):
-  res = []
-  res.append('Original text: %s' %(text))
-  res.append('Converted text: %s' %(''.join(reversed(list(text)))))
-  return res
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port='5000')
