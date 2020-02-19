abc008adbfa16c8459c4783d6a6965309ddf8a75
lipengfei <hudalpf@163.com>
2020-02-20 01:39:04 +0800

    音乐推荐系统

--- src/music_recommendation/main.py
+++ src/music_recommendation/main.py
@@ -4,13 +4,41 @@ from flask import Flask, request, render_template, Markup
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
-def demo():
+@app.route('/index.html', methods=['GET', 'POST'])
+def recommendation1():
   if request.method == 'GET':
     return render_template('index.html', input_text = '', res_text = '')
   else:
     inputText = request.form.get("input_text")
     resText = Markup(formatRes(reverseText(inputText)))
     return render_template('index.html', input_text = inputText, res_text = resText)
+
+@app.route('/rec2.html', methods=['GET', 'POST'])
+def recommendation2():
+  if request.method == 'GET':
+    return render_template('rec2.html', input_text = '', res_text = '')
+  else:
+    inputText = request.form.get("input_text")
+    resText = Markup(formatRes(reverseText(inputText)))
+    return render_template('rec2.html', input_text = inputText, res_text = resText)
+
+@app.route('/rec3.html', methods=['GET', 'POST'])
+def recommendation3():
+  if request.method == 'GET':
+    return render_template('rec3.html', input_text = '', res_text = '')
+  else:
+    inputText = request.form.get("input_text")
+    resText = Markup(formatRes(reverseText(inputText)))
+    return render_template('rec3.html', input_text = inputText, res_text = resText)
+
+@app.route('/rec4.html', methods=['GET', 'POST'])
+def recommendation4():
+  if request.method == 'GET':
+    return render_template('rec4.html', input_text = '', res_text = '')
+  else:
+    inputText = request.form.get("input_text")
+    resText = Markup(formatRes(reverseText(inputText)))
+    return render_template('rec4.html', input_text = inputText, res_text = resText)
 
 def formatRes(textList):
   return '<p>' + '</p><p>'.join(textList) + '</p>'
