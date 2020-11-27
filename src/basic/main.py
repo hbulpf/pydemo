4c2a186e07ad493f80de4e56f2b5b9087aa6ca2c
LiPF <hudalpf@163.com>
2020-11-28 00:02:21 +0800

    最优雅的判断是否回文的方法

--- src/basic/main.py
+++ src/basic/main.py
@@ -4,6 +4,10 @@ def test01():
     a = int('1' + '1')
     print(a)
 
+
+"""
+判断是否是回文
+"""
 def isPalindrome(str):
     i = 0
     while i < (len(str) + 1)/2:
@@ -12,5 +16,15 @@ def isPalindrome(str):
         i+=1
     return True
 
+"""
+最优雅的判断是否回文的方法
+"""
+def isPalindrome2(str):
+    return str == str[::-1]
+
+
 print(isPalindrome('AABAA'))
 print(isPalindrome('AAEBAA'))
+
+print(isPalindrome2('AABAA'))
+print(isPalindrome2('AAEBAA'))
