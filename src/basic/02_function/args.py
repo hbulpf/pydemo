5c88a1b29c98425e84e6911faa28d24d790c82bc
lipengfei <hudalpf@163.com>
2021-03-12 23:48:49 +0800

    函数使用

--- src/basic/02_function/args.py
+++ src/basic/02_function/args.py
@@ -20,24 +20,31 @@ print(add_end2()) # 这里产生了问题 ['END', 'END']
 def person1(name, age, *args, city, job):
     print(name, age, args, city, job)
 
-# 下面调用未给出命名关键字 报错：TypeError: person1() missing 2 required keyword-only arguments: 'city' and 'job'
+# 调用未给出命名关键字 报错：TypeError: person1() missing 2 required keyword-only arguments: 'city' and 'job'
 # person1('Jack', 24, 'Male','Beijing', 'Engineer')
-# 下面调用未给出命名关键字 报错：TypeError: person1() missing 2 required keyword-only arguments: 'city' and 'job'
+# 调用未给出命名关键字 报错：TypeError: person1() missing 2 required keyword-only arguments: 'city' and 'job'
 # person1('Jack', 24, 'Beijing', 'Engineer')
 
+
+
+# 定义person函数使用 位置参数+默认参数+可变参数+关键字参数+命名关键字参数
 def person(a, b=20, *ar, city, job='student', **kw):
 	print('name: ', a, 'age:', b, 'birth:', ar, 'city:', city, 'job:', job, 'other:', kw)
-
+# 测试 位置参数+可变参数+关键字参数+命名关键字参数
 bir = (98, 7, 16)
 inf = {'major': 'maths', 'school': 'zju'}
 person('Pan', 20, *bir, city='guangzhou', job='undergra', **inf)
 
+# 测试 位置参数+可变参数+关键字参数+命名关键字参数
 inf = {'city': 'guangzhou', 'major': 'maths', 'school': 'zju'}
 person('Pan', 20, *bir, **inf)
 
+# 测试 位置参数+可变参数+关键字参数+命名关键字参数
 inf = {'city': 'guangzhou', 'job': 'undergra', 'major': 'maths', 'school': 'zju'}
 person('Pan', 20, *bir, **inf)
 
+# 测试 位置参数+关键字参数+命名关键字参数
 person('Pan', 20, **inf)
 
+# 测试 位置参数+命名关键字参数
 person('Pan', 20, city='gz')
