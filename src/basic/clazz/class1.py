d8a6e43ebc7d398a7991e6b2630ae262c9b817b8
lipengfei <hudalpf@163.com>
2021-03-14 21:42:29 +0800

    面向对象

--- src/basic/clazz/class1.py
+++ src/basic/clazz/class1.py
@@ -1,4 +1,4 @@
-class Student(object):
+class Student1(object):
 
 	def __init__(self, name, score):
 		self.name = name
@@ -17,3 +17,30 @@ class Student(object):
 	
 	def print_grade(self):
 		print('%s: %s' % (self.name, self.get_grade()))
+
+lisa = Student1('Lisa', 99)
+bart = Student1('Bart', 59)
+print(lisa.name, lisa.get_grade())
+print(bart.name, bart.get_grade())
+
+class Student2(object):
+	
+	def __init__(self, name, gender):
+		self.name = name
+		self.__gender = gender
+	
+	def get_gender(self):
+		return self.__gender
+		
+	def set_gender(self, gender):
+		self.__gender = gender
+	
+	def print_age(self):
+		print('%s: %s' % (self.name, self.age))		
+		
+class Student3(object):
+	count = 0
+	
+	def __init__(self, name):
+		self.name = name
+		Student3.count += 1				
--- src/basic/clazz/class3.py
+++
@@ -1,6 +0,0 @@
-class Student(object):
-	count = 0
-	
-	def __init__(self, name):
-		self.name = name
-		Student.count += 1		
