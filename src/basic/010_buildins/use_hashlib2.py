b2978931ce0fda345f5de5c0a5fb355eaa44227c
lipengfei <hudalpf@163.com>
2021-04-09 00:23:03 +0800

    内建模块

--- src/basic/010_buildins/use_hashlib2.py
+++ src/basic/010_buildins/use_hashlib2.py
@@ -1,43 +1,7 @@
 # -*- coding: utf-8 -*-
 import hashlib, random
 
-md5 = hashlib.md5()
-md5.update('how to use md5 in python hashlib?'.encode('utf-8'))
-print(md5.hexdigest())
-
-
-sha1 = hashlib.sha1()
-sha1.update('how to use sha1 in '.encode('utf-8'))
-sha1.update('python hashlib?'.encode('utf-8'))
-print(sha1.hexdigest())
-
-
-# 设计一个验证用户登录的函数，根据用户输入的口令是否正确，返回True或False
-db = {
-    'michael': 'e10adc3949ba59abbe56e057f20f883e',
-    'bob': '878ef96e86145580c38c87f0410ad153',
-    'alice': '99b1c2188db85afee403b1536010c2c9'
-}
-
-def login(user, password):
-    pass
-# 测试:
-assert login('michael', '123456')
-assert login('bob', 'abc999')
-assert login('alice', 'alice2008')
-assert not login('michael', '1234567')
-assert not login('bob', '123456')
-assert not login('alice', 'Alice2008')
-print('ok')    
-
-
-
 # 根据用户输入的登录名和口令模拟用户注册，计算更安全的MD5
-db = {}
-
-def register(username, password):
-    db[username] = get_md5(password + username + 'the-Salt')
-    
 def get_md5(s):
     return hashlib.md5(s.encode('utf-8')).hexdigest()
 
@@ -55,6 +19,7 @@ db = {
 
 def login(username, password):
     user = db[username]
+    password = password + user.salt
     return user.password == get_md5(password)
 
 # 测试:
