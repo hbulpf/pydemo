dd134761abea8014fe4c99696bfce9085e2eebdc
RunAtWorld <hudalpf@163.com>
2021-07-04 13:13:30 +0800

    图像标注

---
+++ src/app/draw_box_cv.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# _*_coding:utf-8_*_
+
+"""
+@Time : 2021/7/4 12:16
+@Author: hudal
+@File: draw_box_cv.py
+@Project: PyCharm
+"""
---
+++ src/app/draw_box_matplot.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# _*_coding:utf-8_*_
+
+"""
+@Time : 2021/7/4 12:17
+@Author: RunAtWorld
+@File: draw_box_matplot.py
+@Project: PyCharm
+"""
