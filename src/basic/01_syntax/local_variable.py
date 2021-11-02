7546e8dd4c16dd37c1d06af5b175a3b98a087c62
RunAtWorld <hudalpf@163.com>
2021-11-02 22:19:30 +0800

    python的重要细节

--- src/basic/01_syntax/local_variable.py
+++ src/basic/01_syntax/local_variable.py
@@ -41,3 +41,73 @@ print(int('10', 8))
 print(int('10', 10))
 print(int('10', 16))
 print(int('10', 3))
+
+
+print("=====")
+if ():
+    print(1)
+if {}:
+    print(2)
+if []:
+    print(3)
+if set():
+    print(4)
+if 0:
+    print(5)
+
+print("=====")
+if () is ():
+    print(1)
+if {} is {}:
+    print(2)
+if [] is []:
+    print(3)
+if set() is set():
+    print(4)
+if '' is '':
+    print(5)
+
+
+print("=====")
+if () == ():
+    print(1)
+if {} == {}:
+    print(2)
+if [] == []:
+    print(3)
+if set() == set():
+    print(4)
+if '' == '':
+    print(5)
+
+print("=====")
+print(type(()))
+print(type({}))
+print(type([]))
+print(type(set()))
+print(type(''))
+print(type(0))
+
+print("=====")
+print(id(()))
+print(id({}))
+print(id([]))
+print(id(set()))
+print(id(''))
+print(id(0))
+
+print("=====")
+a = ()
+b = ()
+print(id(a))
+print(id(b))
+
+a = ""
+b = ''
+print(id(a))
+print(id(b))
+
+a = []
+b = []
+print(id(a))
+print(id(b))
