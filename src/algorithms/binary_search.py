2d22bfcf75268a9c29aa10e24b3250ad33fee384
RunAtWorld <hudalpf@163.com>
2021-11-19 00:04:01 +0800

    二分查找

--- src/algorithms/binary_search.py
+++ src/algorithms/binary_search.py
@@ -7,3 +7,28 @@
 @File: binary_search.py
 @Project: PyCharm
 """
+
+
+def binary_search(arr, key):
+    left = 0
+    right = len(arr) - 1
+    while left <= right:
+        # 特别注意这里的 <=
+        mid = (left + right) // 2
+        if arr[mid] < key:
+            left = mid
+        elif arr[mid] > key:
+            right = mid
+        else:
+            return mid
+    return -1
+
+
+def test1():
+    arr = [4, 2, 5, 2, 8, 0, -1, 3, 7]
+    arr.sort()
+    print(arr, ":", binary_search(arr, 5))
+    print(arr, ":", binary_search(arr, 2))
+
+if __name__ == '__main__':
+    test1()
