84d75107ba84ec176d14b8348e36b933d160769b
RunAtWorld <hudalpf@163.com>
2021-11-29 00:37:25 +0800

    洗牌算法

--- code_python/leetcode_algo_low/design/shuffle_arr..py
+++ code_python/leetcode_algo_low/design/shuffle_arr..py
@@ -8,24 +8,52 @@
 @Project: PyCharm
 """
 from typing import List
+import random
 
 
 class Solution:
+    """
+    等概率选择每个位置应该填哪个数。
+    """
 
     def __init__(self, nums: List[int]):
-        pass
-    
+        self.nums = nums
+
     def reset(self) -> List[int]:
-        return []
+        return self.nums
 
     def shuffle(self) -> List[int]:
-        return []
+        """
+        使用random函数
+        """
+        copy_nums = self.nums[:]
+        for i in range(len(copy_nums)):
+            j = random.randrange(i,len(copy_nums))
+            copy_nums[i], copy_nums[j] = copy_nums[j], copy_nums[i]
+        return copy_nums
+
+    def shuffle2(self) -> List[int]:
+        """
+        使用python的random.shuffle()函数
+        """
+        copy_nums = self.nums[:]
+        random.shuffle(copy_nums)
+        return copy_nums
 
 
 if __name__ == '__main__':
-    solution = Solution([1, 2, 3])
+    solution = Solution([1, 2, 3, 4, 5, 6, 7, 8])
     solution.shuffle()
     solution.reset()
+    print("====")
+    solution.shuffle()
+
+    print("++++")
+
+    solution = Solution([-6, 10, 184])
+    solution.shuffle()
+    solution.reset()
+    print("====")
     solution.shuffle()
 
 # Your Solution object will be instantiated and called as such:
