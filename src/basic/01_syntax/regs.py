5b622872a1ff92214dd41fb4a52c8dc12557fffa
RunAtWorld <hudalpf@163.com>
2021-12-07 23:07:59 +0800

    批量改名

--- src/basic/01_syntax/regs.py
+++ src/basic/01_syntax/regs.py
@@ -7,3 +7,20 @@
 @File: regs.py
 @Project: PyCharm
 """
+import re
+
+
+def is_valid_email(addr):
+    reg = re.compile(r'^[\w.]+@(\w+\.)+\w+$')
+    ok = reg.match(addr)
+    print("addr:%s,%s" % (addr, ok))
+    return ok
+
+
+if __name__ == '__main__':
+    print(re.split(r'[\s,;]+', 'a,b;; c  d'))
+    assert is_valid_email('someone@gmail.com')
+    assert is_valid_email('bill.gates@microsoft.com')
+    assert not is_valid_email('bob#example.com')
+    assert not is_valid_email('mr-bob@example.com')
+    print('ok')
--- src/basic/07_io/file_path.py
+++ src/basic/07_io/file_path.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 
 def traverse_dir(file_dir):
@@ -20,5 +21,22 @@ def query_file_name():
             print('-' * 50)
 
 
+def batch_rename_file(dir_path):
+    """
+    批量重命名文件
+    """
+    old_names = os.listdir(dir_path)
+    for old_name in old_names:
+        if old_name != sys.argv[0]:  # 代码本身文件路径，防止脚本文件放在path路径下时，被一起重命名
+            if "_" in old_name:
+                parts = old_name.split("_")
+                q_num = '0' * (4 - len(parts[0][1:])) + parts[0][1:]
+                prefix = q_num + '_' + parts[0][0]
+                new_name = prefix + '_' + parts[1]
+                print(os.path.join(dir_path, old_name), " ===> ", os.path.join(dir_path, new_name))
+                os.rename(os.path.join(dir_path, old_name), os.path.join(dir_path, new_name))
+
+
 if __name__ == '__main__':
     traverse_dir('.')
+    # batch_rename_file(r'F:\src_sfyh\LeetcodeEveryday\code_python\solution')
