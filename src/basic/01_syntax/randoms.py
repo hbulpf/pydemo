33ade627b05af95ca544f22e8593a92d205eec52
RunAtWorld <hudalpf@163.com>
2021-12-13 22:02:19 +0800

    快速排序算法

---
+++ src/basic/01_syntax/randoms.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# _*_coding:utf-8_*_
+
+"""
+@Time : 2021/12/13 21:34
+@Author: RunAtWorld
+@File: randoms.py
+"""
+import random
+
+if __name__ == '__main__':
+    for i in range(10):
+        rand = random.randint(0,10)
+        print(rand)
