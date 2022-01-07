7fd399e5fc73a6457b40b3ef715223b4b01f2551
RunAtWorld <hudalpf@163.com>
2022-01-07 13:17:09 +0800

    中级-找分割点

--- code_python/solution/0278_L_firstBadVersion.py
+++ code_python/solution/0278_L_firstBadVersion.py
@@ -20,16 +20,14 @@ def isBadVersion(version):
 
 class Solution:
     def firstBadVersion(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
         left = 1
         right = n
         mid = 0
         while left < right:
+            # 特别注意这里的 <
             mid = (right + left) // 2
             if isBadVersion(mid):
+                # 特别注意这里的 mid，不是mid+1
                 right = mid
             else:
                 left = mid + 1
