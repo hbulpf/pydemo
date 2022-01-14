20cd80328f44dcff82cef5164c47a774a06e81b9
RunAtWorld <hudalpf@163.com>
2022-01-15 00:17:23 +0800

    无重复字符的最长子串

--- code_python/solution/0003_M_lengthOfLongestSubstring.py
+++ code_python/solution/0003_M_lengthOfLongestSubstring.py
@@ -2,8 +2,13 @@
 # _*_coding:utf-8_*_
 
 """
+3. 无重复字符的最长子串
+
 @Time : 2022/1/14 23:22
 @Author: RunAtWorld
 @File: 0003_M_lengthOfLongestSubstring.py
-@Project: PyCharm
 """
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        pass
