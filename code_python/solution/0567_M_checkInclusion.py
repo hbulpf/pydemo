7946de1e626e4047d82137f4a6ec679b63e50453
LiPF <hudalpf@163.com>
2022-01-15 23:49:01 +0800

    567. 字符串的排列

--- code_python/solution/0567_M_checkInclusion.py
+++ code_python/solution/0567_M_checkInclusion.py
@@ -11,7 +11,7 @@
 
 
 class Solution:
-    def checkInclusion1(self, s1: str, s2: str) -> bool:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
         """
         O(n) = n * m
         滑动窗口法，依次检测s2中 len(s1) 长度的子串是否和 s1等价
@@ -20,9 +20,13 @@ class Solution:
         s2_len = len(s2)
         if s2_len < s1_len:
             return False
-        for i in range(s2_len - s1_len + 2):
-            r = i + s1_len - 1
-            if self.is_equal(s1, s2[i: r + 1]):
+        # for i in range(s2_len - s1_len + 2):
+        #     r = i + s1_len - 1
+        #     if self.is_equal(s1, s2[i: r + 1]):
+        #         return True
+        # 以下是滑动窗口的标准模板用法
+        for r in range(s1_len, s2_len + 1):
+            if self.is_equal(s1, s2[r - s1_len: r]):
                 return True
         return False
 
@@ -32,18 +36,23 @@ class Solution:
         """
         if len(s1) != len(s2):
             return False
-        s1_dict = dict()
-        for c in s1:
-            if c in s1_dict:
-                s1_dict[c] +=1
-            else:
-                s1_dict[c] = 1
-        for c in s2:
-            if c in s1_dict and s1_dict[c] > 0:
-                s1_dict[c] -= 1
-            else:
-                return False
-        return True
+        # s1_dict = dict()
+        # for c in s1:
+        #     if c in s1_dict:
+        #         s1_dict[c] +=1
+        #     else:
+        #         s1_dict[c] = 1
+        # for c in s2:
+        #     if c in s1_dict and s1_dict[c] > 0:
+        #         s1_dict[c] -= 1
+        #     else:
+        #         return False
+        dic1 = [0] * 26
+        dic2 = [0] * 26
+        for i in range(len(s1)):
+            dic1[ord(s1[i]) - ord('a')] += 1
+            dic2[ord(s2[i])- ord('a')] += 1
+        return dic1 == dic2
 
     def test_is_equal(self):
         print(self.is_equal("acsb", "adsc"))
@@ -56,10 +65,10 @@ if __name__ == '__main__':
     s2 = "eidbaooo"
     print(solution.checkInclusion(s1, s2))
 
-    s1= "ab"
+    s1 = "ab"
     s2 = "eidboaoo"
     print(solution.checkInclusion(s1, s2))
 
-    s1= "abdcexe"
+    s1 = "abdcexe"
     s2 = "cedxaeb"
     print(solution.checkInclusion(s1, s2))
