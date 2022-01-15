0279b0d34ca9feb1137d10a2952e6b84b4d1f28d
LiPF <hudalpf@163.com>
2022-01-16 00:32:21 +0800

    733. 图像渲染

---
+++ code_python/solution/0733_L_floodFill.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# _*_coding:utf-8_*_
+
+"""
+733. 图像渲染
+@Time : 2022/1/16 00:02
+@Author: RunAtWorld
+@File: 0733_L_floodFill.py
+"""
+from typing import List
+
+
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+        pass
